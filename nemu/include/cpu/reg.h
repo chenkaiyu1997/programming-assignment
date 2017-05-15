03792e09faf1fea4308b583971e121dd301d97b9
tracer-ics2015 <tracer@njuics.org>
2017-05-15 21:07:52 +0800

    >  compile NEMU 15307130233 kylechen Linux kylearch 4.10.13-1-ARCH #1 SMP PREEMPT Thu Apr 27 12:15:09 CEST 2017 x86_64 GNU/Linux  21:07:52 up  7:08,  1 user,  load average: 1.16, 0.57, 0.37 d2433ed1513469187ce6db4e9ff4a0c32ecca384

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -22,10 +22,16 @@ typedef union {
 	} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
-	struct {
-		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-		swaddr_t eip;
-	};
+#define eax gpr[0]._32
+#define ecx gpr[1]._32
+#define edx gpr[2]._32
+#define ebx gpr[3]._32
+#define esp gpr[4]._32
+#define ebp gpr[5]._32
+#define esi gpr[6]._32
+#define edi gpr[7]._32
+
+    swaddr_t eip;
 
 	union{
 		struct{
