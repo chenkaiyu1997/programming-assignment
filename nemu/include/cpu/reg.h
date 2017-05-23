f4458aa310f1b9ca956ecd7f8986ad0d72437211
tracer-ics2015 <tracer@njuics.org>
2017-05-23 18:33:39 +0800

    >  compile NEMU 15307130233 kylechen Linux kylearch 4.10.13-1-ARCH #1 SMP PREEMPT Thu Apr 27 12:15:09 CEST 2017 x86_64 GNU/Linux  18:33:39 up 22:24,  1 user,  load average: 0.47, 0.31, 0.22 8886f631132accfe497d87f724755d04f78b0995

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,7 +14,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
+typedef union {
 	union {
 		uint32_t _32;
 		uint16_t _16;
@@ -22,17 +22,15 @@ typedef struct {
 	} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
-
-	// uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-#define eax gpr[0]._32
-#define ecx gpr[1]._32
-#define edx gpr[2]._32
-#define ebx gpr[3]._32
-#define esp gpr[4]._32
-#define ebp gpr[5]._32
-#define esi gpr[6]._32
-#define edi gpr[7]._32
-	swaddr_t eip;
+    #define eax gpr[0]._32
+    #define ecx gpr[1]._32
+    #define edx gpr[2]._32
+    #define ebx gpr[3]._32
+    #define esp gpr[4]._32
+    #define ebp gpr[5]._32
+    #define esi gpr[6]._32
+    #define edi gpr[7]._32
+    swaddr_t eip;
 
 	union{
 		struct{
@@ -56,7 +54,6 @@ typedef struct {
 		};
 		uint32_t eflags;
 	};
-
 
 } CPU_state;
 
