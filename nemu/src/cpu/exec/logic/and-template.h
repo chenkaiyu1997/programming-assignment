700756bb5b3759401b8e42130cf9ff81be2ce936
tracer-ics2015 <tracer@njuics.org>
2017-06-01 21:38:38 +0800

    >  compile NEMU 15307130233 kylechen Linux kylearch 4.11.2-1-ARCH #1 SMP PREEMPT Mon May 22 06:53:49 CEST 2017 x86_64 GNU/Linux  21:38:38 up 1 day,  5:24,  1 user,  load average: 0.33, 0.21, 0.20 bcd5a77e68deae9691c022019a6dda1f657af6dd

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -6,8 +6,8 @@ static void do_execute () {
 	DATA_TYPE result = op_dest->val & op_src->val;
 	OPERAND_W(op_dest, result);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	UPDATE_FLAGS(result);
+	cpu.CF = cpu.OF = 0;
 
 	print_asm_template2();
 }
