44af3d76dcd8315f66ad8d9ec135b062d414d058
tracer-ics2015 <tracer@njuics.org>
2017-06-01 22:07:15 +0800

    >  compile NEMU 15307130233 kylechen Linux kylearch 4.11.2-1-ARCH #1 SMP PREEMPT Mon May 22 06:53:49 CEST 2017 x86_64 GNU/Linux  22:07:15 up 1 day,  5:53,  1 user,  load average: 0.65, 0.40, 0.31 d5dcc91836215f43d0e0c92d5d5326c510ca99d6

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,9 +1,14 @@
 #include "prefix/prefix.h"
 
 #include "data-mov/mov.h"
+#include "data-mov/cmovcc.h"
+#include "data-mov/movsx.h"
+#include "data-mov/movzx.h"
 #include "data-mov/push.h"
+#include "data-mov/pop.h"
 #include "data-mov/xchg.h"
 
+#include "arith/adc.h"
 #include "arith/add.h"
 #include "arith/dec.h"
 #include "arith/inc.h"
@@ -13,6 +18,7 @@
 #include "arith/idiv.h"
 #include "arith/div.h"
 #include "arith/cmp.h"
+#include "arith/sbb.h"
 #include "arith/sub.h"
 
 #include "control/call.h"
@@ -27,11 +33,16 @@
 #include "logic/not.h"
 #include "logic/xor.h"
 #include "logic/sar.h"
+#include "logic/setcc.h"
 #include "logic/shl.h"
 #include "logic/shr.h"
+#include "logic/shld.h"
 #include "logic/shrd.h"
 
+#include "string/cmps.h"
+#include "string/movs.h"
 #include "string/rep.h"
+#include "string/stos.h"
 
 #include "misc/misc.h"
 
