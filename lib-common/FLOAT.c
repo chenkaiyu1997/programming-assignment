31778470c64b5a7da11240bbcd746437671fb5f5
tracer-ics2015 <tracer@njuics.org>
2017-06-01 16:45:35 +0800

    >  compile NEMU 15307130233 kylechen Linux kylearch 4.11.2-1-ARCH #1 SMP PREEMPT Mon May 22 06:53:49 CEST 2017 x86_64 GNU/Linux  16:45:35 up 1 day, 31 min,  1 user,  load average: 0.25, 0.75, 0.67 379113cf8726673c4b333f4c8f9befd839aaee58

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -5,45 +5,39 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	long long A = Fabs(a);
-	long long B = Fabs(b);
-	int c;
-	FLOAT ans = 0;;
-	A <<= 16;
-	B <<= 16;
-	c = 16;
-	while(A != 0) {
-		if (A >= B) {
-			A = A - B;
-			ans = ans | (1 << c);
-		}
-		if (c == 0) break;
-		B = B >> 1;
-		c --;
-	}
-	if ((a < 0 &&  b > 0) || (a > 0 && b < 0)) ans = -ans;
-	return ans;
+    long long A = Fabs(a) << 16;
+    long long B = Fabs(b) << 16;
+    int c = 16;
+    FLOAT ans = 0;
+    for(; A; B>>=1, c--) {
+        if (A >= B) {
+            A = A - B;
+            ans = ans | (1 << c);
+        }
+        if (c == 0) break;
+    }
+    if ((long long)a*(long long)b < 0) ans = -ans;
+    return ans;
 }
 
 FLOAT f2F(float a) {
-	int i, uf, m, e, s, ans;
-	uf = *(int*)&a;
-	m = uf & ((1 << 23) - 1);
-	e = ((uf >> 23) & ((1 << 8) - 1)) - 127;
-	s = uf >> 31;
-	ans = 1;
-	for(i = 1; i <= e + 16; ++ i) {
-		ans = (ans << 1) + ((m & (1 << 22)) >> 22);
-		if (ans < 0) return 0x80000000u;
-		m = m << 1;
-	}
-	if (s != 0) ans = (~ans) + 1;
-	return (FLOAT)(ans);
+    int i, uf, m, e, s, ans;
+    uf = *(int*)&a;
+    m = uf & ((1 << 23) - 1);
+    e = ((uf >> 23) & ((1 << 8) - 1)) - 127;
+    s = uf >> 31;
+    ans = 1;
+    for(i = 1; i <= e + 16; ++ i) {
+        ans = (ans << 1) + ((m & (1 << 22)) >> 22);
+        if (ans < 0) return 0x80000000u;
+        m = m << 1;
+    }
+    if (s != 0) ans = (~ans) + 1;
+    return (FLOAT)(ans);
 }
 
 FLOAT Fabs(FLOAT a) {
-	FLOAT result = a<0?-a:a;
-	return result;
+    return a < 0 ? -a : a;
 }
 
 FLOAT sqrt(FLOAT x) {
